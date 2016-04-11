6a044599180b6be3f1059b95fed7fbac09f98639
thisisaaronland <aaron@mapzen.com>
2016-04-11 06:57:14 +0000

    docs and setup script

---
+++ ubuntu/setup-gearmand.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo apt-get update
+sudo apt-get install -y
+
+sudo apt-get install gearman
+
+exit 0
---
+++ ubuntu/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+sudo apt-get install -y php5-gearman
+
+exit 0
