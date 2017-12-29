94543d91e3d3ed60ba94d37e7e48745b8add358b
Dan Phiffer <dan@phiffer.org>
2017-12-28 20:52:43 -0800

    :cold_sweat: do it the hard way

--- ubuntu/setup-gearmand.sh
+++ ubuntu/setup-gearmand.sh
@@ -1,6 +1,36 @@
 #!/bin/sh
 
 sudo apt-get update
-sudo apt-get install -y gearman php5-gearman
+sudo apt-get install -y uuid-dev libevent-dev gperf libboost-all-dev php7.0-dev
+
+cd /tmp
+wget https://github.com/gearman/gearmand/releases/download/1.1.18/gearmand-1.1.18.tar.gz
+tar xfz gearmand-1.1.18.tar.gz
+cd -
+
+cd /tmp/gearmand-1.1.18/
+./configure
+make
+sudo make install
+cd -
+
+cd /tmp
+wget https://github.com/wcgallego/pecl-gearman/archive/master.zip
+unzip master.zip
+-
+
+cd /tmp/pecl-gearman-master/
+phpize
+./configure
+make
+make install
+cd -
+
+cd /tmp
+rm gearmand-1.1.18.tar.gz
+rm -rf gearmand-1.1.18
+rm master.zip
+rm -rf pecl-gearman-master
+cd -
 
 exit 0
--- ubuntu/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sudo apt-get update
-sudo apt-get upgrade -y
-
-sudo apt-get install -y php5-gearman
-
-exit 0
